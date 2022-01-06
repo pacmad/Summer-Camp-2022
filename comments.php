c63b5da1299ad51043b7b60d8bc17a1bc5078ee8
PierreDubosq1 <pierre1.dubosq@epitech.eu>
2022-01-06 16:22:56 +0100

    Add ID field

--- comments.php
+++ comments.php
@@ -4,6 +4,7 @@ function add_comment(int $article_id, string $user, string $message)
 {
     $comments = json_decode(file_get_contents("data/comments.json"));
     $comments[] = array(
+        'id' => count($comments),
         'article' => $article_id,
         'user' => $user,
         'date' => strtotime("now"),
