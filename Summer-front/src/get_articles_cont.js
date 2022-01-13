609002e505fa50346286a4f4f664a2ec82260c22
Eldenbraz <noxuszero@gmail.com>
2022-01-13 14:23:16 +0000

    front testing

--- Summer-front/src/get_articles_cont.js
+++ Summer-front/src/get_articles_cont.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 
 function get_articles() {
 
-    const [data, setData] = useState(null);
+    const [data, setData] = useState('');
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        axios('http://localhost:8080/api/articles/articles.php')
+    useEffect(async () => {
+        await axios('http://localhost:8000/articles/articles.php?get=true')
         .then(response => {
             setData(response.data);
         })
@@ -21,6 +21,17 @@ function get_articles() {
             setLoading(false);
         })
     })
+
+    if (loading) return "Loading...";
+    if (error) return "Error!";
+    
+    return (
+        <div class={style.home}>
+            <h1 class={style.title}>Le Blog de l'été</h1>
+            <img class={style.pp} src="assets/selfie_homme.jpg"></img>
+            <p class={style.title}>Ceci est un test: {data}</p>
+	    </div>
+    );
 }
 
 export default get_articles;
--- Summer-front/src/routes/home/index.js
+++ Summer-front/src/routes/home/index.js
@@ -1,16 +1,14 @@
 import { h } from 'preact';
 import style from './style.css';
-import get_articles from '../../get_articles_cont';
-// import BlogList from './BlogList';
-// import useFetch from './useFetch';
-// https://flatlogic.com/blog/how-to-build-a-blog-on-react/ -> Import les articles
+import get_articles from '../../get_articles_cont.js';
 
 const Home = () => (
-	<div class={style.home}>
-		<h1 class={style.title}>Le Blog de l'été</h1>
-		<img class={style.pp} src="assets/selfie_homme.jpg"></img>
-		<p class={style.title}>Ceci est un test: {get_articles.data}</p>
-	</div>
+	get_articles()
+	// <div class={style.home}>
+	// 	<h1 class={style.title}>Le Blog de l'été</h1>
+	// 	<img class={style.pp} src="assets/selfie_homme.jpg"></img>
+	// 	{/* <p class={style.title}>Ceci est un test: {get_articles}</p> */}
+	// </div>
 );
 
 export default Home;
