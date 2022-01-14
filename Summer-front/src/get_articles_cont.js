6d1e5464def4d284bbccf46698a622e524c7acd1
Eldenbraz <noxuszero@gmail.com>
2022-01-14 14:31:49 +0000

    Homepage

--- Summer-front/src/get_articles_cont.js
+++ Summer-front/src/get_articles_cont.js
@@ -4,34 +4,18 @@ import axios from 'axios';
 
 function get_articles() {
 
-    const [data, setData] = useState('');
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(async () => {
-        await axios('http://localhost:8000/articles/articles.php?get=true')
-        .then(response => {
-            setData(response.data);
-        })
-        .catch(error => {
-            console.error("Error fetching articles: ", error);
-            setError(error);
-        })
-        .finally(() =>{
-            setLoading(false);
-        })
-    })
-
-    if (loading) return "Loading...";
-    if (error) return "Error!";
+    const [test, setTest] = useState({});
     
-    return (
-        <div class={style.home}>
-            <h1 class={style.title}>Le Blog de l'été</h1>
-            <img class={style.pp} src="assets/selfie_homme.jpg"></img>
-            <p class={style.title}>Ceci est un test: {data}</p>
-	    </div>
-    );
+    useEffect(() => {
+        async function fetchData() {
+            var { argos } = await axios.get('http://localhost:8000/articles/articles.php?get=true');
+            if (!argos)
+                argos = await axios.get('http://localhost:8000/articles/articles.php?get=true')
+            await setTest(argos.data.articles);
+        }
+        fetchData();
+    }, []);
+    return (test);
 }
 
 export default get_articles;
--- Summer-front/src/routes/home/index.js
+++ Summer-front/src/routes/home/index.js
@@ -1,14 +1,31 @@
 import { h } from 'preact';
 import style from './style.css';
-import get_articles from '../../get_articles_cont.js';
+import React, {useEffect, useState} from 'react';
+import get_articles from '../../get_articles_cont';
 
-const Home = () => (
-	get_articles()
-	// <div class={style.home}>
-	// 	<h1 class={style.title}>Le Blog de l'été</h1>
-	// 	<img class={style.pp} src="assets/selfie_homme.jpg"></img>
-	// 	{/* <p class={style.title}>Ceci est un test: {get_articles}</p> */}
-	// </div>
-);
+function Home() {
+	const articles = get_articles();
+	return (
+	<div class={style.home}>
+		<h1 class={style.title}>Le Blog de l'été</h1>
+		<img class={style.pp} src="assets/selfie_homme.jpg"></img>
+			<div class={style.article}>
+				<h2>{articles[0]?.title}</h2>
+				<p>{articles[0]?.description}</p>
+				<b>Date: {articles[0]?.date}</b>
+			</div>
+			<div class={style.article}>
+				<h2>{articles[1]?.title}</h2>
+				<p>{articles[1]?.description}</p>
+				<b>Date: {articles[1]?.date}</b>
+			</div>
+			<div class={style.article}>
+				<h2>{articles[2]?.title}</h2>
+				<p>{articles[2]?.description}</p>
+				<b>Date: {articles[2]?.date}</b>
+			</div>
+	</div>
+	);
+}
 
 export default Home;
